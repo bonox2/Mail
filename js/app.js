d9619d10015010723e8134c7aa43bd0b3d8e863d
bonox2 <fontoften@gmail.com>
2021-01-13 21:13:45 -0500

    did all letters

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
+const messages = MESSAGES
 const list = document.getElementById('list')
-console.log(list)
 
 
 
@@ -8,16 +8,22 @@ renderCard(list, {
         "id": 1,
         "phone": "+63 (924) 979-2252",
         "name": "Guss Marvelley",
-        "message": "Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius. Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi. Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.",
+        "text": "Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius. Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi. Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.",
         "avatar": "https://robohash.org/repellendusimpeditnisi.png?size=50x50&set=set1",
         "date": "1609595510000",
         "seen": false
       })
 
 
+      addCards(list, messages)
+function addCards(elem, messages) {
 
-
-
+    for (let i = 0; i < messages.length; i++) {
+        const mail = messages[i];
+        renderCard(elem,mail)
+    }
+    
+}
 
 function renderCard(where, data) {
     const html =
@@ -30,7 +36,7 @@ function renderCard(where, data) {
                     <div class="sender-number">${data.phone}</div>
                 </div>
             </div>
-            <div class="message-info"> ${data.message}</div>
+            <div class="message-info"> ${data.text}</div>
             <div class="date-time">
                 <div class="time">${data.date}</div>
             </div>
